d304cfe037eba17bdf3b3646abc4fc5a056ccfb1
shargeforce <lars.gerber@bluewin.ch>
2020-12-08 20:20:12 +0100

    initial commit

---
+++ src/trim.js
@@ -0,0 +1,5 @@
+var svg = document.getElementsByTagName("svg")[0];
+var bbox = svg.getBBox();
+var viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(" ");
+svg.setAttribute("viewBox", viewBox);
+prompt("Copy to clipboard: Ctrl+C, Enter", svg.outerHTML);
